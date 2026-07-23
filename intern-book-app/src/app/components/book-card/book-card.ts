22a114756e457559ca76d082f6c3b6fd8122a897
sumudu udara <sumudu.udara@thedott.io>
2026-07-23 10:19:19 +0900

    Implement book delete functionality

--- intern-book-app/src/app/components/book-card/book-card.ts
+++ intern-book-app/src/app/components/book-card/book-card.ts
@@ -1,15 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { Book } from '../../types/book.interface';
 
 @Component({
   selector: 'app-book-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './book-card.html',
   styleUrl: './book-card.css',
 })
 export class BookCard {
   @Input() book!: Book;
+  @Output() delete = new EventEmitter<void>();
+
+  onDelete(): void {
+    this.delete.emit();
+  }
 }
